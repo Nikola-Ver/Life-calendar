1ba58c51fa5fd3a54c748b1b6e7104850ae27503
Nikola-Ver <nikolveresh@gmail.com>
2021-01-21 23:12:07 +0300

    done first step

--- front/src/components/Calendar/Calendar.tsx
+++ front/src/components/Calendar/Calendar.tsx
@@ -28,48 +28,26 @@ interface Data {
 
 const Calendar: React.FC<CalendarProps> = (props) => {
   let [currentCell, setCurrentCell] = useState<any>();
-  
-  let arrOfDoneTasksByDays: Array<Data> = [
-    { items: [{name: 'Mood', value: ' awful'}], countTasks: 15, date: {day: 17, month: 1, year: 2021, dayOfWeek: 0}, toDate: {day: 17, month: 1, year: 2021, dayOfWeek: 0}},
-    { items: [{name: 'Mood', value: ' awful'}], countTasks: 2, date: {day: 18, month: 1, year: 2021, dayOfWeek: 1}, toDate: {day: 18, month: 1, year: 2021, dayOfWeek: 1}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 5, date: {day: 19, month: 1, year: 2021, dayOfWeek: 2}, toDate: {day: 19, month: 1, year: 2021, dayOfWeek: 2}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 21, month: 1, year: 2021, dayOfWeek: 3}, toDate: {day: 21, month: 1, year: 2021, dayOfWeek: 3}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 22, month: 1, year: 2021, dayOfWeek: 4}, toDate: {day: 22, month: 1, year: 2021, dayOfWeek: 4}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 23, month: 1, year: 2021, dayOfWeek: 5}, toDate: {day: 23, month: 1, year: 2021, dayOfWeek: 5}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 24, month: 1, year: 2021, dayOfWeek: 6}, toDate: {day: 24, month: 1, year: 2021, dayOfWeek: 6}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 25, month: 1, year: 2021, dayOfWeek: 0}, toDate: {day: 25, month: 1, year: 2021, dayOfWeek: 0}},
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 26, month: 1, year: 2021, dayOfWeek: 1}, toDate: {day: 26, month: 1, year: 2021, dayOfWeek: 1}},
-  ];
-  let arrOfDoneTasksByWeeks: Array<Data> = [
-      {
-        items: [
-            {
-                name: "Dad",
-                value: "- invited me to fish with his friends and my godfather"
-            },
-            {
-                name: "I",
-                value: "- repeated JS & Node\n- at 01:15 AM go to bed\n- updated the FileWatcher program\n- started writing a new React project in ts\n- repeated hooks for react\n- took a walk with my friends (Artem & Andrey)\n- tried to install a program for Artem's dad (program to diagnostic audi with VOG wire)\n- went to bed at 09:00 PM\n- at 11:40 PM woke up"
-            },
-            {
-                name: "Mood",
-                value: " good"
-            }
-        ],
-        countTasks: 10,
-        date: {day: 17, month: 1, year: 2021, dayOfWeek: 0}, toDate: {day: 24, month: 1, year: 2021, dayOfWeek: 0}
-    },
-    { items: [{name: 'Mood', value: ' awful'}], countTasks: 15, date: {day: 17, month: 1, year: 2021, dayOfWeek: 0}, toDate: {day: 24, month: 1, year: 2021, dayOfWeek: 0}},
-  ];
-  let arrOfDoneTasksByMonths: Array<Data> = [
-    { items: [{name: 'Mood', value: ' ideally'}], countTasks: 15, date: {day: 17, month: 1, year: 2021, dayOfWeek: 0}, toDate: {day: 17, month: 1, year: 2021, dayOfWeek: 0}},
-  ];
+  // 0 - days, 1 - weeks, 2 - months
+  let [arrOfDoneTasks, setArrOfDoneTasks] = useState<Array<Array<Data>>>([[], [], []]);
 
+  if (
+    arrOfDoneTasks[0].length === 0 && 
+    arrOfDoneTasks[1].length === 0 && 
+    arrOfDoneTasks[2].length === 0
+  ) {
+    fetch('http://localhost')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArrOfDoneTasks(data);
+      });
+  }
+       
   const arrOfElements: Array<any> = [];
-  const arrOfDoneTasks: Array<Data> = props.viewType === 0 ? arrOfDoneTasksByDays : props.viewType === 1 ? 
-                                    arrOfDoneTasksByWeeks : arrOfDoneTasksByMonths;
 
-  arrOfDoneTasks.forEach((outElement, index) => {
+  arrOfDoneTasks[props.viewType].forEach((outElement, index) => {
     let mood = 'normal';
     outElement.items.forEach(innerElement => {
       if (innerElement.name.toUpperCase() === 'MOOD') {
@@ -85,7 +63,7 @@ const Calendar: React.FC<CalendarProps> = (props) => {
 
     function getCellInfo(index: number): void {
       let viewComponents: any = [];
-      arrOfDoneTasks[index].items.forEach(e => {
+      arrOfDoneTasks[props.viewType][index].items.forEach(e => {
         if (e.name.toUpperCase() !== 'MOOD')
           viewComponents.push(
             <>
