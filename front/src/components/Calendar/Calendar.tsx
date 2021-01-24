90b8014703862d333ca3a0c1eb8719312846c427
Nikola-Ver <nikolveresh@gmail.com>
2021-01-24 17:19:19 +0300

    added delete content button

--- front/src/components/Calendar/Calendar.tsx
+++ front/src/components/Calendar/Calendar.tsx
@@ -32,52 +32,52 @@ const Calendar: React.FC<CalendarProps> = (props) => {
   // 0 - days, 1 - weeks, 2 - months
   let [arrOfDoneTasks, setArrOfDoneTasks] = useState<Array<Array<Data>>>([[], [], []]);
 
-  if (
-    flagFirstIn &&
-    arrOfDoneTasks[0].length === 0 &&
-    arrOfDoneTasks[1].length === 0 &&
-    arrOfDoneTasks[2].length === 0
-  ) {
-    flagFirstIn = false;
-    fetch('./index.js',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setArrOfDoneTasks(data);
-      });
-  }
-
   // if (
-  //   arrOfDoneTasks[0].length === 0 && 
-  //   arrOfDoneTasks[1].length === 0 && 
+  //   flagFirstIn &&
+  //   arrOfDoneTasks[0].length === 0 &&
+  //   arrOfDoneTasks[1].length === 0 &&
   //   arrOfDoneTasks[2].length === 0
-  // ) 
-  // setArrOfDoneTasks([[
-  //     { items: [{name: "Dad",value: "- invited me to fish with his friends and my godfather"},{name: "I",value: "- repeated JS & Node\n- at 01:15 AM go to bed\n- updated the FileWatcher program\n- started writing a new React project in ts\n- repeated hooks for react\n- took a walk with my friends (Artem & Andrey)\n- tried to install a program for Artem's dad (program to diagnostic audi with VOG wire)\n- went to bed at 09:00 PM\n- at 11:40 PM woke up"},{name: "Mood",value: " good"}],countTasks: 10,date: {day: 17, month: 1, year: 2021}, toDate: {day: 17, month: 1, year: 2021} },
-  //     { items: [{name: 'Mood', value: ' awful'}], countTasks: 2, date: {day: 18, month: 1, year: 2021}, toDate: {day: 18, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 5, date: {day: 19, month: 1, year: 2021}, toDate: {day: 19, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 21, month: 1, year: 2021}, toDate: {day: 21, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 22, month: 1, year: 2021}, toDate: {day: 22, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 23, month: 1, year: 2021}, toDate: {day: 23, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 24, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 25, month: 1, year: 2021}, toDate: {day: 25, month: 1, year: 2021}},
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 26, month: 1, year: 2021}, toDate: {day: 26, month: 1, year: 2021}},
-  // ],
-  // [
-  //     { items: [{name: "Dad",value: "- invited me to fish with his friends and my godfather"},{name: "I",value: "- repeated JS & Node\n- at 01:15 AM go to bed\n- updated the FileWatcher program\n- started writing a new React project in ts\n- repeated hooks for react\n- took a walk with my friends (Artem & Andrey)\n- tried to install a program for Artem's dad (program to diagnostic audi with VOG wire)\n- went to bed at 09:00 PM\n- at 11:40 PM woke up"},{name: "Mood",value: " good"}],countTasks: 10,date: {day: 17, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021} },
-  //     { items: [{name: 'Mood', value: ' awful'}], countTasks: 15, date: {day: 17, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021}},
-  // ],
-  // [
-  //     { items: [{name: 'Mood', value: ' ideally'}], countTasks: 15, date: {day: 17, month: 1, year: 2021}, toDate: {day: 17, month: 1, year: 2021}},
-  // ]]);
+  // ) {
+  //   flagFirstIn = false;
+  //   fetch('./index.js',
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //       }
+  //     }
+  //   )
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setArrOfDoneTasks(data);
+  //     });
+  // }
+
+  if (
+    arrOfDoneTasks[0].length === 0 && 
+    arrOfDoneTasks[1].length === 0 && 
+    arrOfDoneTasks[2].length === 0
+  ) 
+  setArrOfDoneTasks([[
+      { items: [{name: "Dad",value: "- invited me to fish with his friends and my godfather"},{name: "I",value: "- repeated JS & Node\n- at 01:15 AM go to bed\n- updated the FileWatcher program\n- started writing a new React project in ts\n- repeated hooks for react\n- took a walk with my friends (Artem & Andrey)\n- tried to install a program for Artem's dad (program to diagnostic audi with VOG wire)\n- went to bed at 09:00 PM\n- at 11:40 PM woke up"},{name: "Mood",value: " good"}],countTasks: 10,date: {day: 17, month: 1, year: 2021}, toDate: {day: 17, month: 1, year: 2021} },
+      { items: [{name: 'Mood', value: ' awful'}], countTasks: 2, date: {day: 18, month: 1, year: 2021}, toDate: {day: 18, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 5, date: {day: 19, month: 1, year: 2021}, toDate: {day: 19, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 21, month: 1, year: 2021}, toDate: {day: 21, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 22, month: 1, year: 2021}, toDate: {day: 22, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 23, month: 1, year: 2021}, toDate: {day: 23, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 10, date: {day: 24, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 25, month: 1, year: 2021}, toDate: {day: 25, month: 1, year: 2021}},
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 30, date: {day: 26, month: 1, year: 2021}, toDate: {day: 26, month: 1, year: 2021}},
+  ],
+  [
+      { items: [{name: "Dad",value: "- invited me to fish with his friends and my godfather"},{name: "I",value: "- repeated JS & Node\n- at 01:15 AM go to bed\n- updated the FileWatcher program\n- started writing a new React project in ts\n- repeated hooks for react\n- took a walk with my friends (Artem & Andrey)\n- tried to install a program for Artem's dad (program to diagnostic audi with VOG wire)\n- went to bed at 09:00 PM\n- at 11:40 PM woke up"},{name: "Mood",value: " good"}],countTasks: 10,date: {day: 17, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021} },
+      { items: [{name: 'Mood', value: ' awful'}], countTasks: 15, date: {day: 17, month: 1, year: 2021}, toDate: {day: 24, month: 1, year: 2021}},
+  ],
+  [
+      { items: [{name: 'Mood', value: ' ideally'}], countTasks: 15, date: {day: 17, month: 1, year: 2021}, toDate: {day: 17, month: 1, year: 2021}},
+  ]]);
 
   const arrOfElements: Array<any> = [];
 
@@ -204,20 +204,28 @@ const Calendar: React.FC<CalendarProps> = (props) => {
       let viewComponents: any = [];
       let contentTitle = 'Calendar-cell-info-content-title';
       let contentText = 'Calendar-cell-info-content-text Editable';
+      let deleteContentElementButton = 
+        <div 
+          onClick={ (e: any) => { e.target.parentElement.remove() } } 
+          className="Editor-delete Calendar-cell-info-content-element-remove">
+        </div>;
       if (props.viewType === 0) {
         contentTitle += ' Editable';
+      } else {
+        deleteContentElementButton = <></>;
       }
       arrOfDoneTasks[props.viewType][index].items.forEach(e => {
         if (e.name.toUpperCase() !== 'MOOD')
           viewComponents.push(
-            <>
+            <div className="Calendar-cell-info-content-element">
+              { deleteContentElementButton }
               <div className={contentTitle}>
                 {e.name}
               </div>
               <div className={contentText}>
                 {e.value}
               </div>
-            </>
+            </div>
           );
       });
 
